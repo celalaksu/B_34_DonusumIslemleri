79fd2f8484eb5ed5d2e85f97644fc0d7b40fc8e8
celalaksu <32665644+celalaksu@users.noreply.github.com>
2020-05-03 23:53:20 +0300

    Proje dosyası ekle.

---
+++ DonusumIslemleri/DonusumIslemleri/DepthPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DonusumIslemleri
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DepthPage : ContentPage
+    {
+        public DepthPage()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < Device.OnPlatform(12, 12, 18); i++) 
+            { 
+                grid.Children.Insert(0, new Label { TranslationX = i, TranslationY = -i }); 
+            }
+        }
+    }
+}
---
+++ DonusumIslemleri/DonusumIslemleri/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DonusumIslemleri
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void translationPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TranslationPage());
+        }
+
+        private void textEffectsPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TextEffectsPage());
+        }
+
+        private void depthPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DepthPage());
+        }
+
+        private void scalePage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ScalePage());
+        }
+
+        private void rotationPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RotationPage());
+        }
+
+       
+    }
+}
